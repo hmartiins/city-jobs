1cde54dce1803c8d756df6471b244cf0d0ed35e9
Henrique Martins <hmartins224@gmail.com>
2020-04-10 22:12:45 -0300

    Creating the mobile part with react-native

---
+++ mobile/src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+import {
+  StyleSheet,
+  View,
+  Text,
+} from 'react-native';
+
+export default class App extends Component  {
+  render(){
+    return(
+      <View>
+        <Text>
+          City Info
+        </Text>
+      </View>
+    );
+  }
+}

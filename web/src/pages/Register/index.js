38a9f2927a1d57123ebdf362b350a38978d0ebf1
Henrique Martins <hmartins224@gmail.com>
2020-04-11 01:37:18 -0300

    Functional web registration

--- web/src/pages/Register/index.js
+++ web/src/pages/Register/index.js
@@ -9,23 +9,57 @@ import img1 from './img/img.svg'
 import './style.css'
 
 export default function Register(){
+
+   const[name, setName] = useState();
+   const[email, setEmail] = useState();
+   const[password, setPassword] = useState();
+   const [passwordConfirm, setPasswordConfirm] = useState();
+   
+   const history = useHistory();
+
+   async function handleRegister(e){
+      e.preventDefault();
+      
+      const data = {
+         name,
+         email,
+         password,
+      }
+      try {
+         if(password == passwordConfirm){
+            await api.post('auth/register', data);
+            
+            alert(`Cadastrado com sucesso!`).
+            console.log('Cadastrado com sucesso no banco');
+
+            history.push('/profile');
+         }
+         else if(password != passwordConfirm){
+            alert('As senhas não são iguais');
+         }
+      } catch (err) {
+         console.log(err);
+         alert('Erro no cadastro, verifique os campos e tente novamente');
+      }
+   }
+
    return(
       <div className="container">
          <div className="registerContainer">
             <section className="form">
-               <form>
+               <form onSubmit={handleRegister}>
                   <h2>Cadastro</h2>
                   <div className="inputDiv one">
                      <i className="i">
                         <FaAddressCard size={17} /> 
                      </i>
                      <div className="div">
-                        {/* <h5>Username</h5> */}
-
                         <input 
                            placeholder="nome"
                            type="text" 
                            className="input"
+                           value={name}
+                           onChange={e => setName(e.target.value)}
                         />
                      </div>
                   </div>
@@ -34,12 +68,12 @@ export default function Register(){
                         <FaAt size={17} />
                      </i>
                      <div className="div">
-                        {/* <h5>Username</h5> */}
-
                         <input 
                            placeholder="email"
                            type="email" 
                            className="input"
+                           value={email}
+                           onChange={e => setEmail(e.target.value)}
                         />
                      </div>
                   </div>
@@ -48,11 +82,12 @@ export default function Register(){
                         <FaLock size={16} />
                      </i>
                      <div className="div">
-                        {/* <h5>Password</h5> */}
                         <input  
                            placeholder="senha"
                            type="password" 
                            className="input"
+                           value={password}
+                           onChange={e => setPassword(e.target.value)}
                         />
                      </div>
                   </div>
@@ -61,11 +96,12 @@ export default function Register(){
                         <FaLock size={16} />
                      </i>
                      <div className="div">
-                        {/* <h5>Password</h5> */}
                         <input  
                            placeholder="confirme sua senha"
                            type="password" 
                            className="input"
+                           value={passwordConfirm}
+                           onChange={e => setPasswordConfirm(e.target.value)}
                         />
                      </div>
                   </div>

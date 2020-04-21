3c50e44ea465e8d203a4b59981921eaa3b38025c
Henrique Martins <hmartins224@gmail.com>
2020-04-21 19:01:37 -0300

    Correcting the forgot password css page

--- web/src/pages/FirstsPages/ForgotPassword/index.js
+++ web/src/pages/FirstsPages/ForgotPassword/index.js
@@ -7,7 +7,8 @@ import api from '../../../services/api';
 
 import forgotPassword from './img/forgotPassword.svg';
 
-import './styleForgot.css';
+import classes from './styles.module.css'
+import '../global.css';
 
 export default function ForgotPassword(){
   const[email, setEmail] = useState();
@@ -30,26 +31,26 @@ export default function ForgotPassword(){
   }
 
   return(
-    <div className="container">
-      <div className="img">
+    <div className={classes.container}>
+      <div className={classes.img}>
         <img src={forgotPassword} alt="icon"/>
       </div>
-      <div className="passwordContainer">
-        <section className="form">
+      <div className={classes.passwordContainer}>
+        <section className={classes.form}>
           <form onSubmit={handleForgotPassword}>
             <h2>Esqueci Minha Senha</h2>
-            <div className="text">
-              <span className="textPass">
+            <div className={classes.text}>
+              <span className={classes.textPass}>
                 Esqueceu sua senha ? Não tem problema !<br />
                 Nós iremos enviar um token no email digitado, para poder
                 redefinir a senha da sua conta.
               </span>
             </div>
-            <div className="inputDiv one">
+            <div className={[classes.inputDiv, classes.one].join(' ')}>
               <i className="i">
                 <FaAt size={18} />
               </i>
-              <div className="div">
+              <div className={classes.div}>
                 {/* <h5>Password</h5> */}
                 <input  
                    id= {email}

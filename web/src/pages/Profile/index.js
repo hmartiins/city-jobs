e909939c7d3db1280f7f8e25a2ddb764b79b814e
Henrique Martins <hmartins224@gmail.com>
2020-05-11 23:05:33 -0300

    Creating a component for cards

--- web/src/pages/Profile/index.js
+++ web/src/pages/Profile/index.js
@@ -2,26 +2,17 @@ import React from 'react';
 
 import { useHistory } from 'react-router-dom';
 import { makeStyles } from '@material-ui/core/styles';
-import { 
-   Avatar, 
-   Card, 
-   CardActions, 
-   CardContent,
-   Button, 
-   Typography
-} from '@material-ui/core'
+import { Avatar } from '@material-ui/core'
 import { deepPurple } from '@material-ui/core/colors';
 import { 
    FaFacebook, 
    FaInstagram, 
    FaLinkedin, 
    FaTwitter, 
-   FaRegEnvelope,
-   FaPhone,
-   FaDiscord
 } from 'react-icons/fa';
 
 import NavigationBar from '../NavBar/NavigationBar';
+import Cards from './cards/index.js';
 
 import classes from './styles.module.css';
 
@@ -89,47 +80,7 @@ export default function Profile(){
                </div>
             </div>
          </div>
-         <div className={classes.cardContainer}>
-         <Card className={classes.root}>
-            <CardContent>
-               <Typography className={classes.title} color="textSecondary" gutterBottom>
-                  História fornecida por Henrique Martins
-               </Typography>
-               <Typography variant="h5" component="h2">
-                  História
-               </Typography>
-               <Typography variant="body2" component="p">
-                  <div className={classes.historyText}>
-                     Lorem ipsum, dolor sit amet consectetur adipisicing elit. Suscipit eligendi tenetur nihil porro cupiditate quasi impedit veniam, vel, temporibus adipisci obcaecati qui distinctio eum esse, natus neque quisquam quam officia!
-                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ducimus quo dolor ullam ea, officia nam tempora unde atque, ipsam, sequi nihil. Dicta eligendi unde voluptates quaerat amet vel provident!
-                     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maxime perspiciatis eveniet maiores tenetur velit molestias impedit illo facere architecto explicabo, magni deleniti eos quo aliquid sit a fugiat voluptatum est?
-                  </div>
-               </Typography>
-            </CardContent>
-         </Card>
-         <Card className={classes.root2}>
-            <CardContent>
-               <Typography className={classes.title} color="textSecondary" gutterBottom>
-                  Contatos fornecidos por Henrique Martins
-               </Typography>
-               <Typography variant="h5" component="h2">
-                  Entre em contato
-               </Typography>
-               <div className={classes.contato}>
-                  <FaRegEnvelope className={classes.iconEmail} color="#adadad"/>
-                  <span className={classes.iconsTitleCntt}>Email:</span>
-                  <p className={classes.textIconsCntt}>hmartins224@gmail.com</p><br/>
-                  <FaPhone className={classes.iconEmail} color="#adadad"/>
-                  <span className={classes.iconsTitleCntt}>Telefone:</span>
-                  <p className={classes.textIconsCntt}>(13) 99667-0465</p><br/>
-                  <FaDiscord className={classes.iconEmail} color="#adadad"/>
-                  <span className={classes.iconsTitleCntt}>Discord:</span>
-                  <p className={classes.textIconsCntt}>Henriquinho dos Game #06391</p>
-                  <br />
-               </div>
-            </CardContent>
-            </Card>
-         </div>
+         <Cards />
       </div>   
    );
 }

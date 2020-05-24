652ee02c27fbae6676bca6fb4ede034da2541bb1
Henrique Martins <hmartins224@gmail.com>
2020-05-24 16:36:55 -0300

    Finalizing the cardAbout

--- web/src/pages/Profile/components/CardAbout/index.js
+++ web/src/pages/Profile/components/CardAbout/index.js
@@ -10,12 +10,23 @@ export default function CardAbout(){
    return(
       <div className={classes.container}>
         <Card className={classes.cardContainer}>
-         <Typography 
-           variant = 'h6' 
-           gutterBottom
-           style={{marginLeft: 30}}
+         <Typography  
+           style={{marginLeft: 30, marginTop: 10, fontSize: 20}}
          >
-            Sobre o usuário
+            Sobre:
+         </Typography>
+         <Typography
+            variant= 'body2'
+            style={{
+               marginTop: 10, 
+               paddingLeft: 22, 
+               color: '#4d4d4d',
+               paddingRight: 10.5
+            }}
+            
+         >
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vel, fugiat est. Sunt rem tempore veritatis tenetur modi similique magnam vitae laborum esse, iure ducimus nam quam culpa exercitationem molestias commodi.
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ipsam possimus magnam ratione quam commodi repellendus, soluta officia in corporis sapiente nesciunt dolor doloremque adipisci quibusda?
          </Typography>
         </Card>
       </div>
